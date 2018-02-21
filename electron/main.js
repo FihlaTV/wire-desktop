d5cb14f07ddb1104a319395a365dc45fae7d8e40
Thomas Fihla <fihlatv@gmail.com>
2018-02-21 01:41:19 +0000

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,6 @@
 /*
- * Wire
- * Copyright (C) 2017 Wire Swiss GmbH
+ * Airlips 
+ * Copyright (C) 2017 Airlips Rich Communication Suite
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
